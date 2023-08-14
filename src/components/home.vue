2559e8c7c0cc882d10a57e66d01a5bade93b1e2c
Moreno Hernakov <mhadn29@gmail.com>
2023-08-14 12:07:46 +0700

    update pagination

--- src/components/home.vue
+++ src/components/home.vue
@@ -8,77 +8,77 @@
                         alt="login form" class="w-75" style="border-radius: 1rem 0 0 1rem;" />
         </div>
         <div class="col-md-7 ">
-          <div class="card p-3">
-            <div class="input-group mb-3">
-              <div class="input-group mb-3">
+          <div class="card h-100">
+            <div class="card-header">
+              <div class="input-group ">
                 <input v-model="todo" type="text" class="form-control" placeholder="add todo..." aria-label="Recipient's username" aria-describedby="button-addon2">
                 <button @click="addTodo()" class="btn btn-outline-success" type="button" id="button-addon2">save</button>
               </div>
             </div>
             <!-- <table  class="table table-striped text-center"> -->
-            <table v-if="todos.data" class="table table-striped text-center mb-4">
-              <thead>
-                <tr>
-                  <th width="5%">#</th>
-                  <th width="45%">Todo</th>
-                  <th width="8%">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(todoo, index) in todos.data" :key="index" :class="` ${todoo.isChecklist? 'bg-success bg-opacity-25' : ''} `">
-                  <th>
-                    <div class="bg-dark bg-opacity-25 shadow rounded-3 p-1">
-                      <input @click="checklist(index)" v-model="todoo.isChecklist" type="checkbox" >
-                    </div>
-                  </th>
-                  <td v-if="todoo.isChecklist && !todoo.onEdited" :id="index"><del>{{ todoo.todo }}</del></td>
-                  <td v-else-if="todoo.onEdited">
-                    <div class="input-group w-50 m-auto shadow">
-                      <input v-model="todoo.todo" type="text" class="form-control form-control-sm" aria-label="Recipients username" aria-describedby="button-addon2">
-                      <div class="input-group-append">
-                        <button @click="editSaved(index)" class="btn btn-sm btn-success" type="button" id="button-addon2">Save</button>
+            <div class="card-body">
+              <table v-if="todos.data" class="table table-striped text-center mb-4">
+                <thead>
+                  <tr>
+                    <th width="5%">#</th>
+                    <th width="45%">Todo</th>
+                    <th width="8%">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(todoo, index) in todos.data" :key="index" :class="` ${todoo.isChecklist? 'bg-success bg-opacity-25' : ''} `">
+                    <th>
+                      <div class="bg-dark bg-opacity-25 shadow rounded-3 p-1">
+                        <input @click="checklist(index)" v-model="todoo.isChecklist" type="checkbox" >
                       </div>
-                    </div>
-                  </td>
-                  <td v-else :id="index">{{ todoo.todo }}</td>
-                  <td class="d-flex justify-content-around">
-                    <button @click="onUpdateTodo(index)" class="btn btn-outline-secondary btn-sm shadow mr-2">Update</button>
-                    <button @click="deleteTodo(todoo._id)" class="btn btn-danger btn-sm shadow ">Delete</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <p v-else class="text-center text-danger">Todo masih kosong, buat todo pertama mu!!</p>
-            <nav aria-label="Page navigation example">
-              <ul class="pagination">
-                <li class="page-item"><a @click="paginationBtn('prev')" :class="`page-link ${!todos.prev_page_url ? 'disabled' : '' }`" >Previous</a></li>
-                <li class="page-item"><a @click="paginationBtn('next')" :class="`page-link ${!todos.next_page_url ? 'disabled' : '' }`" >Next</a></li>
-              </ul>
-              <p>Page {{ todos.current_page }} of {{ todos.last_page }}</p>
-            </nav>
-            <!-- <pagination v-model="todos" :records="500" @paginate="getAllTodo"/> -->
+                    </th>
+                    <td v-if="todoo.isChecklist && !todoo.onEdited" :id="index"><del>{{ todoo.todo }}</del></td>
+                    <td v-else-if="todoo.onEdited">
+                      <div class="input-group w-50 m-auto shadow">
+                        <input v-model="todoo.todo" type="text" class="form-control form-control-sm" aria-label="Recipients username" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                          <button @click="editSaved(index)" class="btn btn-sm btn-success" type="button" id="button-addon2">Save</button>
+                        </div>
+                      </div>
+                    </td>
+                    <td v-else :id="index">{{ todoo.todo }}</td>
+                    <td class="d-flex justify-content-around">
+                      <button @click="onUpdateTodo(index)" class="btn btn-outline-secondary btn-sm shadow mr-2">Update</button>
+                      <button @click="deleteTodo(todoo._id)" class="btn btn-danger btn-sm shadow ">Delete</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <p v-else class="text-center text-danger">Todo masih kosong, buat todo pertama mu!!</p>
+            </div>
+
+            <div class="card-footer d-flex ">
+              <div class="rounded-3" v-for="(page, index) in todos.links" :key="index">
+                <button v-if="page.label == '&laquo; Previous'" @click="paginationBtn(page.url)" :class="`${!page.url? 'disabled' : ''} btn btn-primary rounded-0 border-white`"> Previous </button>
+                <button v-else-if="page.label == 'Next &raquo;'" @click="paginationBtn(page.url)" :class="`${!page.url? 'disabled' : ''} btn btn-primary rounded-0 border-white`"> Next </button>
+                <button v-else @click="paginationBtn(page.url)" :class="`${page.active? 'text-primary bg-light border-primary fw-bold mx-1' : 'border-white'} btn btn-primary rounded-0 `">{{ page.label }}</button>
+              </div>
+            <!-- <p>Page {{ todos.current_page }} of {{ todos.last_page }}</p> -->
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
+
+// import App from '../App.vue'
+
 export default {
   name: 'home-template',
   created() {
     this.getAllTodo()
-    // this.todos.data.forEach(el => {
-      //   console.log(el)
-      // });
-      
     },
-  data:
-    function () {
+  data() {
       return {
-        todo: [],
+        todo: '',
         todos: [],
         url: 'http://127.0.0.1:8000/api/auth/show?page=1'
       }
@@ -107,9 +107,7 @@ export default {
           this.getAllTodo()
           this.$set(this.todos.data[index], 'onEdited', false)
         })
-        .catch(err => {
-          console.log(err.response)
-        }) 
+        .catch(err => console.error(err.response)) 
 
     },
 
@@ -147,16 +145,8 @@ export default {
         }) 
     },
 
-    paginationBtn : function(e){
-      switch(e) {
-        case'prev':
-          this.url = this.todos.prev_page_url 
-          break;
-        case 'next':
-          this.url = this.todos.next_page_url 
-          break;
-      }
-
+    paginationBtn : function(url){
+      this.url = url
       this.getAllTodo()
     },
 
@@ -168,11 +158,12 @@ export default {
       })
         .then((res) => {
           this.todos = res.data
-
+          
           this.todos.data.forEach((e) => {
             this.$set(e, 'onEdited', false) 
           })
-
+          
+          // App.forceUpdate()
           console.log(this.todos)
         })
         .catch(err => {
@@ -181,6 +172,16 @@ export default {
     },
 
     addTodo : function(){
+      if(this.todo === ''){
+        this.$swal({
+          icon: 'warning',
+          text: 'todo tidak boleh kosong',
+          showConfirmButton: false,
+          timer: 900,
+        })
+        return
+      }
+
       this.axios.post('http://127.0.0.1:8000/api/auth/add', {todo : this.todo} ,{
         headers: {
             Authorization: "Bearer " + localStorage.getItem("token")
@@ -230,6 +231,7 @@ export default {
       })
       
     }
+    
   }
   
 }
